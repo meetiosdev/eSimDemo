554d8467950f2f6feedcc9ec71c02b9b3b2b92f1
Swarajmeet Singh <swarajmeet.crebos@gmail.com>
2025-09-04 15:48:52 +0530

    project init

---
+++ eSimDemo/eSimDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  eSimDemoApp.swift
+//  eSimDemo
+//
+//  Created by Swarajmeet Singh on 04/09/25.
+//
+
+import SwiftUI
+
+@main
+struct eSimDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
